49076d06f94af4ec3ef6ee7edb1bc57cc3c4bf82
gura <guraaaaa777@gmail.com>
2026-04-16 15:10:43 +0900

    compile and change file state

--- twitchrunner_client/twitchreunner/src/main/java/twitchrunner/generator.java
+++ twitchrunner_client/twitchreunner/src/main/java/twitchrunner/generator.java
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class generator {
 	ObjectMapper mapper = new ObjectMapper();
 	runner runner = new runner();
-	private String path = "src/main/resources/data.json";
+	private String path = "data.json";
 	void readjson() {
 		try {
 			this.runner = mapper.readValue(new File(path), runner.class);
